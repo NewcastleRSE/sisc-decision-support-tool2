86ffa70d5f95e3959942af95e13ccb1bec698cf1
katecourt <nkc124@ncl.ac.uk>
2021-04-23 08:39:51 +0100

    integrate ngx-leaflet

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import * as L from 'leaflet';
+import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {GeoserverService} from '../geoserver.service';
+import {Map, Control, DomUtil, ZoomAnimEvent, Layer, MapOptions, tileLayer, latLng, LeafletEvent} from 'leaflet';
 
 
 //
@@ -12,43 +12,78 @@ import {GeoserverService} from '../geoserver.service';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements AfterViewInit {
-private map;
-disabilityData;
+export class MapComponent implements OnDestroy{
+  @Output() map$: EventEmitter<Map> = new EventEmitter;
+  @Output() zoom$: EventEmitter<number> = new EventEmitter;
+  @Input() options: MapOptions = {
+    layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      opacity: 0.7,
+      maxZoom: 19,
+      detectRetina: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })],
+    zoom: 11,
+    center: latLng(54.958455,  -1.6178)
+  };
+  public map: Map;
+  public zoom: number;
+  disabilityData;
 
   constructor(private geoserver: GeoserverService) { }
 
- ngAfterViewInit(): void {
-    this.initMap();
-  }
 
-  initMap(): void {
-    this.map = L.map('map', {
-      center: [ 54.958455,  -1.6178 ],
-      zoom: 11
-    });
+  ngOnDestroy() {
+    this.map.clearAllEventListeners;
+    this.map.remove();
+  };
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
+  onMapReady(map: Map) {
+    this.map = map;
+    this.map$.emit(map);
+    this.zoom = map.getZoom();
+    this.zoom$.emit(this.zoom);
 
     this.createDataLayers();
-
-    L.piechartMarker(
-      L.latLng([37.7930, -122.4035]),
-      {
-        data: [
-          { name: 'Apples', value: 25 },
-          { name: 'Oranges', value: 35 },
-          { name: 'Bananas', value: 20 },
-          { name: 'Pineapples', value: 30 }
-        ]
-      }
-    ).addTo(this.map);
   }
+
+  onMapZoomEnd(e: LeafletEvent) {
+    this.zoom = e.target.getZoom();
+    this.zoom$.emit(this.zoom);
+  }
+
+
+
+ // ngAfterViewInit(): void {
+ //    this.initMap();
+ //  }
+ //
+ //  initMap(): void {
+ //    this.map = L.map('map', {
+ //      center: [ 54.958455,  -1.6178 ],
+ //      zoom: 11
+ //    });
+ //
+ //    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ //      maxZoom: 18,
+ //      minZoom: 3,
+ //      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+ //    });
+ //    tiles.addTo(this.map);
+ //
+ //    this.createDataLayers();
+ //
+ //    L.piechartMarker(
+ //      L.latLng([37.7930, -122.4035]),
+ //      {
+ //        data: [
+ //          { name: 'Apples', value: 25 },
+ //          { name: 'Oranges', value: 35 },
+ //          { name: 'Bananas', value: 20 },
+ //          { name: 'Pineapples', value: 30 }
+ //        ]
+ //      }
+ //    ).addTo(this.map);
+ //  }
 
 
 
