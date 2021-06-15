563098a58e3209807a04be906c4aa900a0bde1f3
katecourt <nkc124@ncl.ac.uk>
2021-06-15 09:50:46 +0100

    add missing info to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  GEOSERVERWMS: 'https://sisc-geoserver.azurewebsites.net/geoserver/siss/wms?',
+  GEOSERVERWFS: 'https://sisc-geoserver.azurewebsites.net/geoserver/siss/wfs?',
+  websocket_url: 'https://optimisation-backend.azurewebsites.net'
 };
