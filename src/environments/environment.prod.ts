c9f5dcd7c771d2d7c7db0bd1f57311aacf57ddb0
katecourt <nkc124@ncl.ac.uk>
2021-07-08 09:03:47 +0100

    add serverless function to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   GEOSERVERWMS: 'https://sisc-geoserver.azurewebsites.net/geoserver/siss/wms?',
   GEOSERVERWFS: 'https://sisc-geoserver.azurewebsites.net/geoserver/siss/wfs?',
-  websocket_url: 'https://optimisation-backend.azurewebsites.net'
+  websocket_url: 'https://optimisation-backend.azurewebsites.net',
+  serverless_function_url: 'https://siscfunctionapp.azurewebsites.net/api/oaData?code=2UMNS3JZwdcsR3ALCPa3x3xm6duHLFuKyK2Owarq1M0Cc/LLljNyQg=='
 };
