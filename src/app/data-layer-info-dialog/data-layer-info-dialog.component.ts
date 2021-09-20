dae1e55e2f2eece30870c6a5aaad93d998ef9bfc
katecourt <nkc124@ncl.ac.uk>
2021-09-20 12:10:37 +0100

    info for space syntax

--- src/app/data-layer-info-dialog/data-layer-info-dialog.component.ts
+++ src/app/data-layer-info-dialog/data-layer-info-dialog.component.ts
@@ -17,12 +17,21 @@ text;
           '<p>Density is calculated as the percentage of people in the output area out of all reporting from the whole Local Authority, per km<span class="sup">2</span></p>';
         break;
       }
-      // case constant_expression2: {
-      //   //statements;
-      //   break;
-      // }
+      case 'imd': {
+       this.text = '<p>Index of Multiple Deprivation is a measure of relative levels of deprivation across households grouped into Lower-layer Super Output Areas. It ' +
+         'is not available at an Output Area level of granularity. Areas are grouped into bands from 1 (most deprived) to 10 (least deprived).</p>';
+       break;
+      }
+      case 'space': {
+        this.text = "<p>Space syntax is used here as a way of modelling patterns of movement through a city informed by its spatial layout. " +
+          "<p>'To movement' refers to the likelihood of people <strong>moving to</strong> a location from all others.</p>" +
+          "<p>'Through movement' refers to the likelihood of people <strong>passing through</strong> a location. It represents " +
+          "people using the shortest routes from all locations in the area to all other locations.</p>" +
+          "<p> Add reference here...</p>";
+        break;
+      }
       default: {
-        // statements;
+        this.text = 'No data information is currently available for this layer.';
         break;
       }
     }
