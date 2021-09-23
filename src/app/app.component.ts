0cf370cbd4c4cba22237c34b62d6bdb95e1db396
katecourt <nkc124@ncl.ac.uk>
2021-09-23 14:48:55 +0100

    change tab title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sisc-decision-support-tool2';
+
+  public constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.setTitle('SISC');
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
