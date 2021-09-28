daf9f26d8d5d168735339fcd30fd4bcbbab5eb1c
katecourt <nkc124@ncl.ac.uk>
2021-09-28 13:59:05 +0100

    create scatter chart with jitter

--- src/app/test-highcharts/test-highcharts.component.ts
+++ src/app/test-highcharts/test-highcharts.component.ts
@@ -6,52 +6,78 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./test-highcharts.component.scss']
 })
 export class TestHighchartsComponent implements OnInit {
+  // Make all the colors semi-transparent so we can see overlapping dots
+ colors = Highcharts.getOptions().colors.map(function (color) {
+    return Highcharts.color(color).setOpacity(0.5).get();
+  });
+
+
   Highcharts: typeof Highcharts = Highcharts;
   chartOptions: Highcharts.Options = {
     chart: {
-      type: "spline"
+      type: 'scatter'
     },
+
+    colors: this.colors,
+
     title: {
-      text: "Monthly Average Temperature"
+      text: 'Scatter chart with jitter'
     },
     subtitle: {
-      text: "Source: WorldClimate.com"
+      text: ''
     },
-    xAxis:{
-      categories:["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    xAxis: {
+      categories: ['Workplace', 'Residence', 'Over 65 years old']
     },
     yAxis: {
-      title:{
-        text:"Temperature °C"
+      title: {
+        text: 'Measurements'
       }
     },
-    tooltip: {
-      valueSuffix:" °C"
-    },
-    series: [
-      {
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,26.5, 23.3, 18.3, 13.9, 9.6]
-      },
-      {
-        name: 'New York',
-        data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5]
-      },
-      {
-        name: 'Berlin',
-        data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-      },
-      {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+    plotOptions: {
+      scatter: {
+        showInLegend: false,
+        jitter: {
+          x: 0.24,
+          y: 0
+        },
+        marker: {
+          radius: 2,
+          symbol: 'circle'
+        },
+        tooltip: {
+          pointFormat: 'Measurement: {point.y:.3f}'
+        }
       }
-    ]
+    },
+    series: [{
+      name: 'Workplace',
+      data: this.getTestData(0)
+    }, {
+      name: 'Residence',
+      data: this.getTestData(1)
+    }, {
+      name: 'over 65 years',
+      data: this.getTestData(2)
+    }]
   };
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getTestData(x) {
+    var data = [],
+      off = 0.2 + 0.2 * Math.random(),
+      i;
+    for (i = 0; i < 200; i++) {
+      data.push([x, off + (Math.random() - 0.5) * (Math.random() - 0.5)]);
+    }
+    console.log(data)
+    return data;
+  }
+
+
 
 }
