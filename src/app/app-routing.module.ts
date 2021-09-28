397bf35c5590c43de9809c59f437dcaae3130e20
katecourt <nkc124@ncl.ac.uk>
2021-09-28 13:44:56 +0100

    add highcharts line graph

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MapComponent} from './map/map.component';
+import {TestHighchartsComponent} from './test-highcharts/test-highcharts.component';
 
 
 const routes: Routes = [
   { path: '', component: MapComponent},
+  { path: 'test', component: TestHighchartsComponent}
 ];
 
 @NgModule({
