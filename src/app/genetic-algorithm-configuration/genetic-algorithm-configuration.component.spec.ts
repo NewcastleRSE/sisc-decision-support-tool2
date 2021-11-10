a47d06cc39d87e021b3ea0ea8f9dcf061be539bc
katecourt <nkc124@ncl.ac.uk>
2021-11-10 11:28:27 +0000

    fix algorithm config test imports

--- src/app/genetic-algorithm-configuration/genetic-algorithm-configuration.component.spec.ts
+++ src/app/genetic-algorithm-configuration/genetic-algorithm-configuration.component.spec.ts
@@ -2,15 +2,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GeneticAlgorithmConfigurationComponent } from './genetic-algorithm-configuration.component';
 
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatIconModule} from '@angular/material/icon';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+
+let loader: HarnessLoader;
+
 describe('GeneticAlgorithmConfigurationComponent', () => {
   let component: GeneticAlgorithmConfigurationComponent;
   let fixture: ComponentFixture<GeneticAlgorithmConfigurationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GeneticAlgorithmConfigurationComponent ]
+      declarations: [ GeneticAlgorithmConfigurationComponent ],
+      imports: [MatIconModule,
+      MatDialogModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
+    fixture = TestBed.createComponent(GeneticAlgorithmConfigurationComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
   }));
 
   beforeEach(() => {
