1b49ccc154c220535c100e9e0749b6515b03f8a5
katecourt <nkc124@ncl.ac.uk>
2021-12-10 11:58:01 +0000

    start setting up services tests

---
+++ src/app/database.service.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DatabaseService } from './Database.service';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('DatabaseService', () => {
+  let service: DatabaseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    service = TestBed.inject(DatabaseService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- src/app/geoserver.service.spec.ts
+++ src/app/geoserver.service.spec.ts
@@ -1,3 +1,28 @@
+import { TestBed } from '@angular/core/testing';
 
+import { GeoserverService } from './geoserver.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('GeoserverService', () => {
+  let service: GeoserverService;
+
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    //service = TestBed.inject(GeoserverService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new GeoserverService(httpClientSpy);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
 // get tile layer called with correct layer from parameters
 // legend to 2 decimal places
